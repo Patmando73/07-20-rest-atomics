d8a51bf90cd33dd0677cec1eb4aa929c77ac7964
Sam Stephen <samueljstephen@gmail.com>
2015-07-20 13:58:55 -0500

    working on adding users to database and listing users.

--- controllers/main.rb
+++ controllers/main.rb
@@ -1,13 +1,26 @@
+#home page
+get "/home" do
+  erb :"main/home"
+end
+
+
 #list users
 get "/users" do
-  @user_array = User.all
-  erb :"users"
+  @users = User.all
+  json_array = []
+  @users.each do |d|
+    json_array << d.json_format
+  end
+  json json_array
 end
+
 
 
 #new user
 get "/users/new" do
-
+  @password = BCrypt::Password.create(params["password"])
+  @new_user = User.create({email: params["email"], password: @password})
+  json @new_user.json_format
 end
 
 
@@ -18,10 +31,12 @@ post "/users" do
 end
 
 
+
 #delete user
 delete "/users/:id" do
 
 end
+
 
 
 #edit users
@@ -30,13 +45,16 @@ get "/users/:id/edit" do
 end
 
 
+
 #update users
 put "/users/:id" do
 
 end
 
 
+
 #show users
 get "/users/:id" do
 
 end
+
