2fcfff15a6f8b7ccffbd75d584d7e9821f80fdcb
Sam Stephen <samueljstephen@gmail.com>
2015-07-20 15:57:30 -0500

    function showUsers - list all users in db

--- public/global.js
+++ public/global.js
@@ -3,6 +3,7 @@
  */
 
 
+
 function goHome() {
     window.location.replace("/home");
 }
@@ -24,3 +25,29 @@ function hideModal() {
 
 var close = document.getElementById('close');
 close.addEventListener('click', hideModal);
+
+
+function showUsers() {
+    var request = new XMLHttpRequest();
+    var route = '/users';
+    request.open('get', route);
+    request.addEventListener('load', function() {
+       document.getElementById('list-of-users').innerText = '';
+       for (var i= 0; i < this.response.length; i++ ) {
+           var user = this.response[i];
+           var newLiId = user.id;
+           var newLi = document.createElement('li');
+           newLi.id = newLiId;
+           newLi.dataId = user.id;
+           newLi.innerText = user.email;
+           var Li = document.getElementById('list-of-users');
+           Li.appendChild(newLi);
+           document.getElementById(newLiId).onclick = showThisUser;
+       }
+    });
+    request.responseType = "json";
+    request.send();
+}
+
+document.getElementById('list-users-link').onclick = showUsers;
+
