2b9b2356160b2b5e4fff76318cb997c772d122d5
Patrick Carlton <pcarlton73@gmail.com>
2015-07-20 15:03:06 -0500

    normalize.css and nav-toggle.js added

---
+++ public/nav-toggle.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Patmando73 on 7/20/15.
+ */
+
+
+// show nav
+document.getElementsByTagName('nav')[0].style.display = 'block';
+
+// hide nav
+//document.getElementsByTagName('nav')[0].style.display = 'none';
+
+var buttonNav = document.getElementById('nav');
+buttonNav.addEventListener('click', toggle);
+
+function toggle(){
+    var navigation = document.getElementsByTagName('nav');
+    if ( navigation[0].style.display != 'none') {
+        navigation[0].style.display = 'none';
+    } else {
+        navigation[0].style.display = 'block';
+    }
+}
