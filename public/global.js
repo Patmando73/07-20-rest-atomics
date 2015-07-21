713d6b90dcbfaeca08c6ee17796f6c5498e91152
Sam Stephen <samueljstephen@gmail.com>
2015-07-21 11:40:01 -0500

    delete works - modify wip...

--- public/global.js
+++ public/global.js
@@ -46,6 +46,7 @@ function showUsers(event) {
             newLi.innerText = user.email;
             var Li = document.getElementById('list-of-users');
             Li.appendChild(newLi);
+            document.getElementById(newLiId).style.cursor = 'pointer';
             document.getElementById(newLiId).onclick = showThisUser;
         }
     });
@@ -63,15 +64,14 @@ function showThisUser() {
         var user = this.response;
         document.getElementById('list-of-users').innerText = '';
 
+       // var deleteUser = document.getElementById('list-of-users');
+        user_id = user.id;
 
-        var modifyUser = document.getElementById('list-of-users');
-        taco = user.id;
+        $("#list-of-users").append("<form method='post' id='delete-form'>delete<input type='hidden' name='_method' value='delete'><input type='submit'> </form>");
 
-        $("#list-of-users").append("<form method='post' id='formyform'> <input type = 'radio' name = 'thing' value = 'taco'>modify<input type='hidden' name='_method' value='put'><input type='submit'> </form>");
+        deleteForm =  document.getElementById("delete-form");
 
-        formyform =  document.getElementById("formyform")
-
-        formyform.action = "/users/" + user.id + "/edit";
+        deleteForm.action = "/users/" + user.id;
 
         //var modifyUserInput1 = document.createElement("input");
         //var modifyUserInput2 = document.createElement("input");
@@ -85,15 +85,15 @@ function showThisUser() {
         //modifyUser.input = modifyUserInput2;
         //modifyUser.innerText = "Modify " + user.email;
         //modifyUser.href = "/users/"+ user.id +"/edit";
-        var theUl = document.getElementById('list-of-users');
-        theUl.appendChild(modifyUser);
+        //var theUl = document.getElementById('list-of-users');
+        //theUl.appendChild(deleteUser);
         //document.getElementById(newLiId).onclick = modifyTheUser;
 
-        var deleteUser = document.createElement('a');
-        deleteUser.innerText = "Delete " + user.email;
-        deleteUser.href = "/users/"+ user.id;
-        var theUl = document.getElementById('list-of-users');
-        theUl.appendChild(deleteUser);
+        //var deleteUser = document.createElement('a');
+        //deleteUser.innerText = "Delete " + user.email;
+        //deleteUser.href = "/users/"+ user.id;
+        //var theUl = document.getElementById('list-of-users');
+        //theUl.appendChild(deleteUser);
         //document.getElementById(newLiId).onclick = deleteTheUser;
 
     });
